8268f7b42bbf42525a294c5b8e46f0c6078602b2
UNG <rnwldnd7248@gmail.com>
2021-11-08 23:13:49 +0900

    second commit

--- src/main/java/persistence/dao/Established_SubjectDAO.java
+++ src/main/java/persistence/dao/Established_SubjectDAO.java
@@ -1,5 +1,95 @@
 package main.java.persistence.dao;
 
-public class Established_SubjectDAO {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import main.java.persistence.dto.Established_SubjectDTO;
+import main.mapper.EstablishedMapper;
+
+public class Established_SubjectDAO{
+
+	//Singleton pattern
+	    private static SqlSessionFactory sqlSessionFactory;
+	     public Established_SubjectDAO() {
+	          if(sqlSessionFactory == null){ 
+            InputStream inputStream; 
+            try{ 
+                inputStream = Resources.getResourceAsStream("resource.Configuration.xml"); 
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream); 
+            }catch (IOException e){ 
+                 e.printStackTrace(); 
+      	  } 
+        } 
+	     
+	     }
+	
+	     public int Insert(Established_SubjectDTO dto) {
+	    	       SqlSession session = sqlSessionFactory.openSession();
+	    	       EstablishedMapper mapper = session.getMapper(EstablishedMapper.class);      
+
+      session.getConfiguration().addMapper(EstablishedMapper.class);
+      
+      		      
+      		      //need to know mapper.insertData return value
+      		      int res =mapper.insertData(dto);
+      		      session.commit();
+      		      session.close();
+				return res;
+	    	 
+	     }
+	     
+	     
+	     public int Update(Established_SubjectDTO dto) {
+	    	 SqlSession session = sqlSessionFactory.openSession();
+	    	 EstablishedMapper mapper = session.getMapper(EstablishedMapper.class);      
+
+	    	 dto=mapper.selectbyId(2);
+	    	 
+	    	 //change the data using setter
+	    	 
+	    	int res= mapper.updateData(dto);
+	    	 session.commit();
+	    	 session.close();
+	    	 return res;
+	     }
+	     
+	     
+	     public List<Established_SubjectDTO >selectDataAll(){
+	    	 
+	    	  SqlSession session = sqlSessionFactory.openSession();
+	    	 EstablishedMapper mapper = session.getMapper(EstablishedMapper.class);      
+	    	 List<Established_SubjectDTO> list =null;
+	    	 list =mapper.selectDataAll();
+	    	 return list;
+	    	 
+	     }
+	     
+	         public List<Established_SubjectDTO >selectBygrade(){
+	    	 
+	    	  SqlSession session = sqlSessionFactory.openSession();
+	    	 EstablishedMapper mapper = session.getMapper(EstablishedMapper.class);      
+	    	 List<Established_SubjectDTO> list =null;
+	    	 list =mapper.selectDataAll();
+	    	 return list;
+	    	 
+	     }
+	         
+	            public List<Established_SubjectDTO >selectByprof(){
+	    	 
+	    	  SqlSession session = sqlSessionFactory.openSession();
+	    	 EstablishedMapper mapper = session.getMapper(EstablishedMapper.class);      
+	    	 List<Established_SubjectDTO> list =null;
+	    	 list =mapper.selectDataAll();
+	    	 return list;
+	    	 
+	     }
+	    	 
+
+	
 }
--- src/main/mapper/EstablishedMapper.java
+++ src/main/mapper/EstablishedMapper.java
@@ -1,29 +1,36 @@
-
+package main.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import main.java.persistence.dto.Established_SubjectDTO;
 
-
 public interface EstablishedMapper {
 
-		@Select("select * from Established_Subject")
+	//need to revise all of these query 
+	
+	@Select("select * from Established_Subject")
 
 	public List<Established_SubjectDTO> selectDataAll();
+	
+	@Select("select * from Established_Subject")
+
+	public List<Established_SubjectDTO> selectBygrade();
+
+	@Select("select * from Established_Subject")
+	
+	public List<Established_SubjectDTO> selectByprof();
 
 	
-
 	@Select("select * from Established_Subject where id=#{id}")
 
-	public Established_SubjectDTO selectData(String arg);
+	public Established_SubjectDTO selectbyId(int arg);
 
 
-
+//If the insertData return 0 , or null need to change the return type
 	@Insert("insert into Established_Subject values(#{id}, #{name}, #{pass}, now())")
 
 	public int insertData(Established_SubjectDTO dto);
@@ -33,15 +40,7 @@ public interface EstablishedMapper {
 	@Update("update membertab set name=#{name}, pass=#{pass} where id=#{id}")
 
 	public int updateData(Established_SubjectDTO dto);
-
 	
-
-	@Delete("delete from membertab where id=#{id}")
-
-	public int deleteData(String arg);
-
-}
-
-
+	
 
 }
